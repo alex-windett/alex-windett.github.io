af5381574e8f2e9a3d89852796094ce9cc837d22
Alex Windett <alexwindett@hotmail.com>
2014-10-01 17:38:14 +0100

    refactor code

--- js/app.js
+++ js/app.js
@@ -48,9 +48,14 @@ $(function(){
   });
 
 
-  $("#skill_1").tooltip({
-    content: "JavaScript",
-    track: true
-  }).tooltip("open");
+// Google Anaylitics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55311551-1', 'auto');
+  ga('send', 'pageview');
+
 
 });
