53f9a80f5020ac755b59491dd04d8cd8cd97d065
Alex Windett <alexwindett@hotmail.com>
2014-10-02 21:49:37 +0100

    responsive

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(function(){
       $(this).attr("href"),
       {
         duration: 950,   
-        offset: {"left": 0, "top": -30}
+        offset: {"left": 0, "top": -70}
       }
     );
   });
