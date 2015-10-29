f2851f94f89536202d83b1ec1b336798c84c4ba9
Alex Windett <alex@bigfish.co.uk>
2015-10-29 10:14:52 +0000

    adding in script links

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,3 @@
 $(document).ready(function(){
 
-	$('.projects article').hide();
-
-    $('.item-wrap').mouseenter(function(){
-    	$(this).children('article').show();
-    }).mouseleave(function(){
-    	$(this).children('article').hide();
-    })
-
-
-    // $('.nav__main--trigger').click(function(event){
-    // 	event.preventDefault();
-    // 	$('.nav__main').animate({width:'toggle'},350).toggleClass('triggered');;
-    // 	$(this).toggleClass('triggered');
-    // });
 });
