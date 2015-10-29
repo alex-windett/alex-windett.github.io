59e1795f40eefc64be99d1eae723e800399c7996
Alex Windett <alex@bigfish.co.uk>
2015-10-29 15:09:07 +0000

    working on nav

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-
+	$('header .burger-bar').on('click', function() {
+		$(this).toggleClass('cross');
+		$('.header-menu').slideToggle().toggleClass('revelaed');;
+	});
 });
