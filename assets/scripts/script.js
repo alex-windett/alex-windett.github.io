8b842e007cdd2c3d664be1bfd4dca116c5bd2639
Alex Windett <alex@bigfish.co.uk>
2015-11-03 16:26:28 +0000

    starting functionality for sliding grid on home page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,46 @@
 $(document).ready(function(){
+
+	// ++++++++++++++++++++++++++++++++
+    // Navigation
+    // ++++++++++++++++++++++++++++++++
 	$('header .burger-bar').on('click', function() {
 		$(this).toggleClass('cross');
 		$('.header-menu').slideToggle().toggleClass('revelaed');;
 	});
+
+
+    // ++++++++++++++++++++++++++++++++
+    // General Helpers
+    // ++++++++++++++++++++++++++++++++
+    $('a.link__removed').click(function(event) {
+        event.preventDefault();
+    });
+
+
+    // ++++++++++++++++++++++++++++++++
+    // Home Page Projects
+    // ++++++++++++++++++++++++++++++++
+
+    var projectItem 	= $('.block--grid li'),
+    	projectDetails	= $('article'),
+    	projectLogo		= $('.project--logo'),
+    	projectPreview	= $('.project--preview');
+
+
+  	projectItem.click(function(){
+  		if ( $(this).hasClass('active') ) {
+
+  			$(this).removeClass('active');
+  				.find(projectLogo).show();
+
+  		} else {
+
+	  		$(this).addClass('active')
+	  		$(this).find(projectLogo).hide();
+	  		$(this).siblings()
+	  			.removeClass('active')
+	  			.find(projectLogo).show();
+
+	  	}
+  	})
 });
