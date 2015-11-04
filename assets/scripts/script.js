ffb91e30075a7e368cca50a770825365f1ffa270
Alex Windett <alex@bigfish.co.uk>
2015-11-04 14:35:12 +0000

    responsifying homepage and updating images

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    var windowWidth = $(window).width();
+        largeScreenSize = 640,
+
 	// ++++++++++++++++++++++++++++++++
     // Navigation
     // ++++++++++++++++++++++++++++++++
@@ -22,28 +25,43 @@ $(document).ready(function(){
     // ++++++++++++++++++++++++++++++++
 
     var projectItem 	= $('.block--grid li'),
-    	projectDetails	= $('article'),
-    	projectLogo		= $('.project--logo'),
-    	projectPreview	= $('.project--preview');
+        projectDetails	= $('article'),
+        projectLogo		= $('.project--logo'),
+        projectPreview	= $('.project--preview');
 
 
   	projectItem.click(function(){
+        projectItem.each(function(index, el) {
+            $(this).css('left', '0')
+        });
+
   		if ( $(this).hasClass('active') ) {
-  			$(this).removeClass('active')
-  				   .find(projectLogo).show();
+  			$(this).removeClass('active').find(projectLogo).show();
+             $(this).css('left', '0')
   		} else {
   			var elementPositon  = $(this).position(),
   				elementIndex	= $(this).index()
 
-	  		$(this).addClass('active')
-	  			   .find(projectLogo).hide();
-	  		$(this).siblings()
-	  			.removeClass('active')
-	  			.find(projectLogo).show();
+	  		$(this).addClass('active').find(projectLogo).hide();
+	  		$(this).siblings().removeClass('active').find(projectLogo).show();
 
-	  		if ( elementIndex > 0 ) {
-			  	$(this).detach().prependTo('.block--grid').fadeIn();
+	  		// Moving all but the first element
+	  		if ( elementIndex > 0 && windowWidth >= largeScreenSize ) {
+                // Very basic but works
+                // $(this).detach().prependTo('.block--grid');
+                alert();
+                var left        = $(this).position().left,
+                    itemWidth   = projectItem.width();
+
+                $(this).animate({
+                    left: "-" + left
+                }, 'fast' );
+                $(this).siblings().animate({
+                    left: itemWidth + 20 // Componsating for extra space
+                }, 'fast' );
 	  		}
+
+
 	  	}
   	})
 });
